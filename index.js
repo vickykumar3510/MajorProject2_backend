ba992de413abb3494794c3d4d71932d4724a6e75
vickykumar3510 <vicky.kumar3510@gmail.com>
2025-12-20 16:23:46 +0530

    chore: code updated

--- index.js
+++ index.js
@@ -399,4 +399,9 @@ app.use((req, res) => {
 });
 
 
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
 module.exports = app;
